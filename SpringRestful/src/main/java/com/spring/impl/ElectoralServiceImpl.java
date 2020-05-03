4f2b5102d831ef3ab15638ab6dd689c1ff543f4a
apeksha2403 <60914765+apeksha2403@users.noreply.github.com>
2020-05-03 13:36:56 +0530

    Electoral service

---
+++ SpringRestful/src/main/java/com/spring/impl/ElectoralServiceImpl.java
@@ -0,0 +1,74 @@
+package com.spring.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.spring.entity.ApplicationEntity;
+import com.spring.entity.UserCredentialsEntity;
+import com.spring.entity.UserEntity;
+import com.spring.json.Application;
+import com.spring.json.User;
+import com.spring.repository.ApplicationRepository;
+import com.spring.repository.ElectoralRepository;
+import com.spring.repository.UserCredentialsRepository;
+import com.spring.repository.UserRepository;
+import com.spring.service.ElectoralServices;
+
+@Service
+public class ElectoralServiceImpl implements ElectoralServices {
+	
+	@Autowired
+	private ApplicationRepository applicationrepo;
+	
+	@Autowired
+	private ElectoralRepository electoral;
+	
+	@Autowired
+	private UserCredentialsRepository userCredentialsRepository;
+	
+	@Autowired
+	private UserRepository userRepository ;
+
+	@Override
+	public Object getApprovedUser(String sessionId) {
+		UserCredentialsEntity userCredentialsEntity = userCredentialsRepository.findBySessionId(sessionId);	
+		if (userCredentialsEntity != null) {
+
+			List<ApplicationEntity> pendingList = applicationrepo.findByApprovedstatus(1);
+			if (pendingList != null) {
+				return pendingList;
+			}
+			else {
+				return "{\"result\": \"failure\",\"message\": \"No Pending request available.\"}";
+			}
+		}
+		return "{\"result\": \"failure\",\"message\": \"Wrong Session Id\"}";
+
+	}
+	
+
+	@Override
+	public Object generateVoterId(Application application, String userid,String sessionId) {
+			ApplicationEntity app = applicationrepo.findByUserid(userid);
+			if(app != null) {
+				app.setVoterId(application.getVoterId());
+			    app.setApprovedStatus(app.getApprovedStatus());
+			    app.setPassedStatus(app.getPassedStatus());
+			    app.setConstituency(app.getConstituency());
+			    app=applicationrepo.save(app);
+				return app;
+			}
+			return null;
+		}
+
+	}
+	
+	
+	
+	
+	
+	
+	
