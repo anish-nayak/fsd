ed837de26f76ea8c23ea60745747b68250673d6f
apeksha2403 <60914765+apeksha2403@users.noreply.github.com>
2020-05-04 09:48:32 +0530

    Electoral Repository added

---
+++ SpringRestful/src/main/java/com/spring/repository/ElectoralRepository.java
@@ -0,0 +1,10 @@
+package com.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.entity.ElectionEntity;
+import com.spring.entity.ElectoralOfficer;
+
+public interface ElectoralRepository extends JpaRepository<ElectoralOfficer, String>{
+
+}
